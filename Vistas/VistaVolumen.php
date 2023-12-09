13501c001db41124727f3191e13cb7335bd10fa0
psamayoa <pamelasamayoa@hotmail.com>
2023-12-08 19:06:00 -0600

    first commit

---
+++ Vistas/VistaVolumen.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Volumen</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../index.php">Conversor</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./VistaVolumen.php">Volumen</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section class="container text-center">
+        <h1 class="text-center">Conversor de volumen</h1><br>
+        <form action="" method="post">
+            <div class="row">
+                <div class="col-3">
+                    <br>
+                    <div class="input-group mb-3">
+                        <label class="input-group-text" for="inputGroupSelect01">Medidas</label>
+                        <select class="form-select" name="medida">
+                            <option selected>Elegir...</option>
+                            <option value="ConvertirLitros">Litros</option>
+                            <option value="ConvertirGalones">Galones</option>
+                            <option value="piesCubicos">Pies Cúbicos</option>
+                            <option value="mililitros">Mililitros</option>
+                            <option value="centimetrosCubicos">Centímetros Cúbicos</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <label>Cantidad</label>
+                    <input type="text" class="form-control" name="cant" required>
+                </div>
+            </div><br>
+            <div class="row">
+                <div class="col-3">
+                    <div class="input-group mb-3">
+                        <label class="input-group-text" for="inputGroupSelect01">A Convertir</label>
+                        <select class="form-select" id="inputGroupSelect01">
+                            <option selected>Elegir...</option>
+                            <option value="">Litros</option>
+                            <option value="">Galones</option>
+                            <option value="">Pies Cúbicos</option>
+                            <option value="">Mililitros</option>
+                            <option value="">Centímetros Cúbicos</option>
+                        </select>
+                    </div>
+                </div><br>
+                <div class="row">
+                    <div class="col-3">
+                        <div class="input-group mb-3">
+                            <button class="btn btn-success" type="submit" id="button-addon1">Conversión</button>
+                            <input type="text" class="form-control" value="">
+                        </div>
+                    </div>
+                </div>
+        </form>
+    </section>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Conversor</title>
+</head>
+
+<body>
+    <br>
+    <h1 class="text-center">Conversor de unidades</h1><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/volumen.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">Volumen</h3>
+                        <a href="./Vistas/VistaVolumen.php" class="btn btn-primary">Convertir</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./img//longitud.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">Longitud</h3>
+                        <a href="#" class="btn btn-primary">Convertir</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/tiempo.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">Tiempo</h3>
+                        <a href="#" class="btn btn-primary">Convertir</a>
+                    </div>
+                </div>
+            </div>
+        </div><br>
+        <div class="row">
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/masa.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">Masa</h3>
+                        <a href="#" class="btn btn-primary">Convertir</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/datos.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">Datos</h3>
+                        <a href="#" class="btn btn-primary">Convertir</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/moneda.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-center">Moneda</h3>
+                        <a href="#" class="btn btn-primary">Convertir</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
