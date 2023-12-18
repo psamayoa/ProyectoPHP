372d4a894e186acd81dce256ca4245e410dff287
María Fernanda Isturiz <135650517+maria-fernanda-isturiz@users.noreply.github.com>
2023-12-18 15:18:05 -0600

    Update index_moneda2.php

--- Vistas/index_moneda2.php
+++ Vistas/index_moneda2.php
@@ -8,6 +8,21 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../index.php">Conversor</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./index_moneda2.php">Volumen</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <section class="container">
         <h1 class="text-center">Cálculo De Moneda</h1><br>
             <form action="" method="POST">
